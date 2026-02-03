27c3ab2523bade0447bd0db6fc7eacfac78651e7
Ghanashyam Khanal <khanalg44@gmail.com>
2026-02-03 09:27:01 -0500

    Add profile picture to home page
    - Move profilepic1.jpg to client/public/images/
    - Add profile picture on right side of home page
    - Create Home.css with responsive layout
    - Display image as circular with shadow
    - Responsive design: image above text on mobile

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import './Home.css';
 
 const Home = () => {
   return (
     <section className="section-content">
-      <h2>I am a quantitative analyst interested in quantum computing and cricket data analysis.</h2>
-      <p>I am a quantitative analyst (quant) currently working at Citigroup, NYC. I hold a PhD in physics where I developed numerical libraries to study the fascinating world of quantum materials. Not only our tool explained the complicated physical properties of real materials, our tool was also shown to have a robust predictive ability. Learn more about my PhD Work <a href="http://hauleweb.rutgers.edu/tutorials/Tutorial_phonons.html" target="_blank" rel="noopener noreferrer">here</a>. I am passionate about cricket data analysis and learning about the world of finance through an economic lens around the world.</p>
+      <div className="home-container">
+        <div className="home-text">
+          <h2>I am a quantitative analyst interested in quantum computing and cricket data analysis.</h2>
+          <p>I am a quantitative analyst (quant) currently working at Citigroup, NYC. I hold a PhD in physics where I developed numerical libraries to study the fascinating world of quantum materials. Not only our tool explained the complicated physical properties of real materials, our tool was also shown to have a robust predictive ability. Learn more about my PhD Work <a href="http://hauleweb.rutgers.edu/tutorials/Tutorial_phonons.html" target="_blank" rel="noopener noreferrer">here</a>. I am passionate about cricket data analysis and learning about the world of finance through an economic lens around the world.</p>
+        </div>
+        <div className="home-image">
+          <img src="/images/profilepic1.jpg" alt="Ghanashyam Khanal" className="profile-pic" />
+        </div>
+      </div>
     </section>
   );
 };
